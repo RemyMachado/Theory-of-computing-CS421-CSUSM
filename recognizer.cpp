edbda9013ff8314f068889a3a7d4a0fc45654e30
RemyMachado <remy.machado@epitech.eu>
2020-02-06 18:58:08 -0800

    [refactor] use given file of the homework instructions

--- recognizer.cpp
+++ recognizer.cpp
@@ -1,40 +1,50 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 
-#define MINIMUM_MAIN_PARAMETERS_COUNT 2
-#define MAIN_PARAMETER_INDEX_TO_TEST 1
+using namespace std;
+
+//-------------------------------------------------------
+// CS421 HW1
+// Write a recognizer in C++ for L = {x | x is a binary number}.
+// Your name: **Anthony Machado
+//-------------------------------------------------------
+
 #define PROGRAM_SUCCESS 0
 #define ALPHABET "01"
 #define RECOGNIZED_MSG "YES IN L"
 #define REJECTED_MSG "NO NOT IN L"
 
 void PrintRecognition() {
-    std::cout << RECOGNIZED_MSG << std::endl;
+    cout << RECOGNIZED_MSG << endl;
 }
 
 void PrintRejection() {
-    std::cout << REJECTED_MSG << std::endl;
+    cout << REJECTED_MSG << endl;
 }
 
 /* Recognizer  for L = { x | x is a binary number } */
-bool IsRecognized(std::string const &element) {
-    // reject if a letter does not belong to the alphabet
-    return element.find_first_not_of(ALPHABET) == std::string::npos;
-}
-
-int main(int argc, char **argv) {
-    // reject if a null parameter was given and exit program
-    if (argc < MINIMUM_MAIN_PARAMETERS_COUNT) {
-        PrintRejection();
-        return PROGRAM_SUCCESS;
+bool recognizer(string s) {
+    // reject if it's an empty string
+    if (s.empty()) {
+        return false;
     }
 
-    // test if the given element is belonging to the language
-    if (IsRecognized(argv[MAIN_PARAMETER_INDEX_TO_TEST])) {
-        PrintRecognition();
-    } else {
-        PrintRejection();
+    // reject if a character does not belong to the alphabet
+    return s.find_first_not_of(ALPHABET) == string::npos;
+}
+
+int main() {
+    string line;
+
+    while (std::getline(cin, line)) {
+        // test if the given element is belonging to the language
+        if (recognizer(line)) {
+            PrintRecognition();
+        } else {
+            PrintRejection();
+        }
     }
 
     return PROGRAM_SUCCESS;
 }
+
